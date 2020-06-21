6e51106aaeac23fc6e4f0591a6d28c57913287c3
Julian Henry <julianplushenry@gmail.com>
2020-06-21 11:50:49 -0500

    Add files via upload

---
+++ src/airscales.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
+import math
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+import geo as ge
+import graph as gr
+import calc as cc
+
+class airscales(cc.calcbot, gr.graphbot):
+    def __init__(slf, geobot, params):
+        slf.grid_cnt = params['grid_cnt'] if 'grid_cnt' in params.keys() else 10
+        slf.window = params['window'] if 'window' in params.keys() else 30
+        slf.model = params['model'] if 'model' in params.keys() else LinearRegression()
+        slf.geo_df = geobot.geo_df
+        cc.calcbot.__init__(slf, slf.geo_df, slf.grid_cnt)
+        gr.graphbot.__init__(slf, slf.geo_df, slf.grid_cnt)
+
+    def vanilla_linreg(slf):
+        return slf.vna_analysis(LinearRegression())
+
+class pfit:
+    def __init__(slf, d):
+        slf.degree = d
+
+    def fit(slf, x, y):
+        return np.polyfit(x, y, slf.degree)
+
+if __name__ == '__main__':
+    realestate_file = 'processed_realestate.csv'
+    raw_df = pd.read_csv(realestate_file)
+    params = {'grid_cnt':4, 'window':5, 'step':1, 'model': LinearRegression() }
+    M = airscales(ge.geobot(raw_df, False), params)
+    dix = M.raw_distrix
+
+    # M.scatter_district(dix[7][0], LinearRegression())
+    # Give function degree d, and return function consuming x,y that returns np.polyfit(x,y,d)
+    # P = pfit(2)
+    vanlinreg = M.vna_analysis(LinearRegression())
+    M.oculos(vanlinreg)
+    M.scatter_all()
+    M.interpolate(vanlinreg)
+    # # M.refract([[1,1,1],[1,1,1],[1,1,1]])
